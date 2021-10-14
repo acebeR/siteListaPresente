6b3e38ac8dd7edf8cdcf0d9781f12348fb94b80b
Rebeca Divina Paula Carvalhedo <rebecadivinapc@gmail.com>
2021-10-14 12:09:58 -0300

    tentando scopo

---
+++ lista-presentes/src/pesquisa/ListaCasal.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class ListaCasal extends Component {
+
+    constructor(props){
+        super(props)
+    }
+
+    render (){
+      return (
+        <div>
+           <div  className='row d-flex flex-row justify-content-center mt-5'>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Noivo</th>
+                        <th scope="col">Noiva</th>
+                        <th scope="col">Lista</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        </div>
+        )
+    }
+}
+
+export default ListaCasal;
--- lista-presentes/src/pesquisa/PesquisaCasal.js
+++ lista-presentes/src/pesquisa/PesquisaCasal.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
 import {PesquisaService} from './PesquisaService.js';
+import ListaCasal from './ListaCasal.js';
 
 class PesquisaCasal extends Component {
 
     constructor(props){
         super(props)
-        this.state = {noiva: '',noivo:''};
+        this.state = {noiva: '',noivo:'',casalPesquisa:''};
         this.casais = [];
+        this.casalPesquisa = [];
         this.PesquisaService = new PesquisaService();
-        // this.pesquisarCasais = this.pesquisarCasais.bind(this);
     }
 
     pesquisarCasais(noiva,noivo){
-        console.log("estoy aqui");
-        console.log(noiva,noivo);
-
         this.PesquisaService.getCasais().then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
-                this.casais.push(doc.data());
+                var casal = doc.data();
+                if(casal.Mulher === noiva && casal.Homem === noivo){
+                    this.casalPesquisa.push(casal);
+                    this.setState({casalPesquisa:{id:casal.id,Mulher:casal.Mulher,Homem:casal.Homem}});
+                }
             });
-            console.log("-----------DENTRO-------------");
-            console.log(this.casais);
         })
         .catch((error) => {
             console.log("Error getCasais: ", error);
@@ -28,42 +28,72 @@ class PesquisaCasal extends Component {
 
     }
 
+    teste(){
+        console.log(this.casalPesquisa);
+        console.log(this.state.casalPesquisa);
+    }
+
+    // pesquisarCasais(noiva,noivo){
+    //     console.log("estoy aqui");
+    //     console.log(noiva,noivo);
+
+    //     this.PesquisaService.getCasais().then((querySnapshot) => {
+    //         querySnapshot.forEach((doc) => {
+    //             this.casais.push(doc.data());
+    //         });
+    //         console.log("-----------DENTRO-------------");
+    //         console.log(this.casais);
+    //     })
+    //     .catch((error) => {
+    //         console.log("Error getCasais: ", error);
+    //     });
+
+    // }
+
 
     render (){
-      return (
-        <div>
-            {/* ======= Resume Section ======= */}
-            <section id='casal' className='casal'>
-            <div className='container'>
+        return (
+            <div>
+                {/* ======= Resume Section ======= */}
+                <section id='casal' className='casal'>
+                <div className='container'>
 
-            <div className='section-title'>
-                <h2>Casal de Noivos</h2>
-                <p>Pesquise pelo nome do noivo e da noiva para que, você convidado, presenteie os noivos!</p>
-                </div>
+                    <div className='section-title'>
+                        <h2>Casal de Noivos</h2>
+                        <p>Pesquise pelo nome do noivo e da noiva para que, você convidado, presenteie os noivos!</p>
+                    </div>
                     <div className="row d-flex flex-row justify-content-center mt-5">
                         <div className="col-lg-5">
                             <div className="card">
                                 <div className="card-body">
-                                   {/* <form onSubmit={this.pesquisarCasais}> */}
-                                        <div className="form-group">
-                                        <input type="text" name="noiva" className="form-control" value={this.state.noiva} onChange={ e => this.setState({noiva: e.target.value}) } placeholder="Noiva"/>
-                                        </div>
-                                        <div className="form-group">
-                                            <input type="text" name="noivo" className="form-control" value={this.state.noivo} onChange={ e => this.setState({noivo: e.target.value}) } placeholder="Noivo"/>
-                                        </div>
-                                        <div className="form-group">
-                                            <button className="btn btn-primary btn-lg btn-block" onClick={() => this.pesquisarCasais(this.state.noiva,this.state.noivo)}>
-                                                Pesquisar
-                                            </button>
-                                        </div>
-                                    {/* </form> */}
+                                    <div className="form-group">
+                                    <input type="text" name="noiva" className="form-control" value={this.state.noiva} onChange={ e => this.setState({noiva: e.target.value}) } placeholder="Noiva"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" name="noivo" className="form-control" value={this.state.noivo} onChange={ e => this.setState({noivo: e.target.value}) } placeholder="Noivo"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <button className="btn btn-primary btn-lg btn-block" onClick={() => this.pesquisarCasais(this.state.noiva,this.state.noivo)}>
+                                            Pesquisar
+                                        </button>
+                                    </div>
+                                    <label value={this.state.casalPesquisa}></label>
+                                    -----{this.state.casalPesquisa}--------
+                                    {console.log(this.state.casalPesquisa)}
                                 </div>
                             </div>
                         </div>
                     </div>
+                    {/* {
+                        if(this.state.casalPesquisa){ 
+                            return <ListaCasal/>
+                        }
+                    } */}
+                    
                 </div>
-            </section>{/* End Resume Section */}
-        </div>
+                    
+                </section>{/* End Resume Section */}
+            </div>
         )
     }
 }
